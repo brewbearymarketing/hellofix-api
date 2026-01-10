00ad4413d6e3fefa8078a9117e56111fd6264856
brewbearymarketing <brewbearymarketing@gmail.com>
2026-01-10 16:46:07 +0800

    Delete pages/api/worker.ts

--- pages/api/worker.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async function worker(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("🧵 WORKER ALIVE");
-  return res.status(200).json({ ok: true });
-}
